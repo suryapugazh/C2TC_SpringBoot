6d8d56865d4a0810335c517e8d0ad6658de1dfcc
Surya <suryapugazh27@gmail.com>
2025-11-05 22:03:49 +0530

    Spring Boot Project Basic Concepts 1 and 2

---
+++ springboot-web-app-2/src/main/java/com/tnsif/springboot_web_app_2/SpringbootWebApp2Application.java
@@ -0,0 +1,13 @@
+package com.tnsif.springboot_web_app_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebApp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebApp2Application.class, args);
+	}
+
+}
