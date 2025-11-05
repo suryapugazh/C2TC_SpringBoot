95394ddc23f468dfd117beec2b3a7bf16bcb6ba3
Surya <suryapugazh27@gmail.com>
2025-11-05 23:11:56 +0530

    Spring Boot Project-based Concepts-5 (Qualifier and Primary Annotation)

---
+++ springboot-di-qualifier/src/test/java/com/tnsif/springboot_di_qualifier/SpringbootDiQualifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tnsif.springboot_di_qualifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDiQualifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
